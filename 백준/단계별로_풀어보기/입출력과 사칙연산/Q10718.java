b78abb5fda31ef7cf0a925b92a8355d1230e8881
Junghun Sim <96688359+092600@users.noreply.github.com>
2022-04-30 15:32:19 +0000

    2022 04 30 / TIL
    append Q10718

---
+++ 백준/단계별로_풀어보기/입출력과 사칙연산/Q10718.java
@@ -0,0 +1,8 @@
+package 백준.단계별로_풀어보기;
+
+public class Q10718 {
+    public static void main(String[] args){
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
