39785c1f8dc72b165042829a7f20df1c2c5273c6
Junghun Sim <96688359+092600@users.noreply.github.com>
2022-05-01 11:15:19 +0000

    2022 05 01 / TIL
    append Q1000

---
+++ 백준/단계별로_풀어보기/입출력과 사칙연산/Q1000.java
@@ -0,0 +1,13 @@
+package 백준.단계별로_풀어보기;
+
+import java.util.*;
+
+public class Q1000 {
+    public static void main(String[] args){
+        Scanner stdin = new Scanner(System.in);
+        int a = stdin.nextInt();
+        int b = stdin.nextInt();
+
+        System.out.println(a + b);
+    }
+}
