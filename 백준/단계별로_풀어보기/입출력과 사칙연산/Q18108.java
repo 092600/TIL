ab9bd26a216b8bd6bcea3c538cdde095330f9bc5
Junghun Sim <96688359+092600@users.noreply.github.com>
2022-05-04 21:52:30 +0000

    2022 05 04 / TIL
    append Q18108

---
+++ 백준/단계별로_풀어보기/입출력과 사칙연산/Q18108.java
@@ -0,0 +1,11 @@
+package 백준.단계별로_풀어보기;
+import java.util.*;
+
+public class Q1001 {
+    public static void main(String[] args){
+        Scanner stdin = new Scanner(System.in);
+        int input = stdin.nextInt();
+
+        System.out.println(input - 543);
+    }
+}
