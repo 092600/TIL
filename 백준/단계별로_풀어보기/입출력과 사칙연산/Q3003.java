19a70ef1ec7e798eec56ae0fae1447d41e7c6b99
Junghun Sim <96688359+092600@users.noreply.github.com>
2022-05-04 21:52:30 +0000

    2022 05 04 / TIL
    append Q3003

---
+++ 백준/단계별로_풀어보기/입출력과 사칙연산/Q3003.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Q3003 {
+    public static void main(String[] args){
+        Scanner stdin = new Scanner(System.in);
+        int[] input = stdin.nextLine();
+
+        System.out.println(Arrays.toString(input));
+    }
+}
