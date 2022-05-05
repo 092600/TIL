ac59f8dc09f1b09d6b4f7c5dd5848cc38ec322e7
Junghun Sim <96688359+092600@users.noreply.github.com>
2022-05-05 11:35:45 +0000

    2022 05 05
    update Q3003

--- 백준/단계별로_풀어보기/입출력과 사칙연산/Q3003.java
+++ 백준/단계별로_풀어보기/입출력과 사칙연산/Q3003.java
@@ -3,8 +3,16 @@ import java.util.*;
 public class Q3003 {
     public static void main(String[] args){
         Scanner stdin = new Scanner(System.in);
-        int[] input = stdin.nextLine();
+        String inp = stdin.nextLine();
+        String[] inpList = inp.split(" ");
+        int[] lst = {1, 1, 2, 2, 2, 8};
 
-        System.out.println(Arrays.toString(input));
+        for (int i = 0 ; i<lst.length; i++){
+            if (lst[i] != Integer.parseInt(inpList[i])){
+                System.out.print(lst[i] - Integer.parseInt(inpList[i]) + " ");
+            } else {
+                System.out.print(0 + " ");
+            }
+        }
     }
 }
