46ec21c946d55541d64860d487fac9b5f86ad7de
Junghun Sim <96688359+092600@users.noreply.github.com>
2022-05-06 16:57:03 +0000

    2022 05 06 / TIL
    append Q10430

---
+++ 백준/단계별로_풀어보기/입출력과 사칙연산/Q10430.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Q10430 {
+    public static void main(String[] args){
+        Scanner stdin = new Scanner(System.in);
+        int A = stdin.nextInt();
+        int B = stdin.nextInt();
+        int C = stdin.nextInt();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
+    }
+}
