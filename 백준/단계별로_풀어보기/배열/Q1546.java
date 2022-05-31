0b7df97f3fb7e2c31054d175d17559aad58cf6db
Junghun Sim <96688359+092600@users.noreply.github.com>
2022-05-31 22:53:33 +0000

    2022 05 31 / TIL
    append Q1546

---
+++ 백준/단계별로_풀어보기/배열/Q1546.java
@@ -0,0 +1,27 @@
+public class Q1546{
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+
+        int k; int n; int max; double sum = 0;
+        k = stdIn.nextInt();
+
+        int[] lst = new int[k];
+        n = stdIn.nextInt();
+
+        lst[0] = max = n;
+
+
+        for (int i=1;i<k;i++){
+            n = stdIn.nextInt();
+            lst[i] = n;
+
+            if (n > max)
+                max = n;
+        }
+
+        for (int l : lst){
+            sum += ((double) l / max ) * 100;
+        }
+        System.out.println((sum / k) * 100 / 100);
+    }
+}
