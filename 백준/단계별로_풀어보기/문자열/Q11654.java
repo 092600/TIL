286d718c9d73ed80b41de575805e19fc536c0676
Junghun Sim <96688359+092600@users.noreply.github.com>
2022-06-03 08:19:36 +0000

    2022 06 03 / TIL
    append Q11654

---
+++ 백준/단계별로_풀어보기/문자열/Q11654.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Q11654{
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+        char in = stdIn.next().charAt(0);
+        System.out.println((int)in);
+
+    }
+}
