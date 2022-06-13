002aced4a8e7241d3b92e858e787fd0db5ce82a3
Junghun Sim <96688359+092600@users.noreply.github.com>
2022-06-13 23:03:02 +0000

    2022 06 13 / TIL
    append Q2941

---
+++ 백준/단계별로_풀어보기/문자열/Q2941.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Q1152{
+    public static void main(String[] args){
+        String[] cro_alphabet_list = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+        String[] alphabet_list = {"a","b","c","d","e","f","g","h","i","j"
+                ,"k","l",'m','n',"o","p","q",'r','s','t',
+                    'u',"v",'w',"x","y","z"};
+        System.out.println(cro_alphabet_list);
+        System.out.println(alphabet_list);
+    }
+}
