ee29921aff7e628eb9f4b44932e8b79d16f39593
Junghun Sim <96688359+092600@users.noreply.github.com>
2022-06-23 14:21:10 +0000

    2022 06 23 / TIL
    append Q4673

---
+++ 백준/단계별로_풀어보기/함수/Q4673.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Q4673 {
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+        int[] tmp_list = new int[10000];
+
+        for (int i = 0; i<10000; i++){
+                
+        }
+
+    }
+}
+
