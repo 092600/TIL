93cd57b9d53843c1bb646ce8bb9610939382f719
Junghun Sim <96688359+092600@users.noreply.github.com>
2022-06-25 15:13:51 +0000

    2022 06 25 / TIL
    update Q4673

--- 백준/단계별로_풀어보기/함수/Q4673.java
+++ 백준/단계별로_풀어보기/함수/Q4673.java
@@ -3,10 +3,22 @@ import java.util.*;
 public class Q4673 {
     public static void main(String[] args){
         Scanner stdIn = new Scanner(System.in);
-        int[] tmp_list = new int[10000];
+        List lst = new ArrayList<Integer>();
+        for (int i = 1; i<=10000; i++){
+            String tmpStr = String.valueOf(i);
+            int tmp = i;
 
-        for (int i = 0; i<10000; i++){
-                
+            for (String t : tmpStr.split("")){
+                tmp += Integer.parseInt(t);
+            }
+
+            lst.add(tmp);
+        }
+
+        for (int i=1;i<=10000; i++){
+            if (!(lst.contains(i))){
+                System.out.println(i);
+            }
         }
 
     }
