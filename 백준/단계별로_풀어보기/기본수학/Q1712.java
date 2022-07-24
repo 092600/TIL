6418824f289afa4ec516cdef82f084d58c47f89f
Junghun Sim <96688359+092600@users.noreply.github.com>
2022-07-24 15:15:46 +0900

    2022 07 24 / TIL
    append 백준/단계별/기본수학 1/Q1712, update README

---
+++ 백준/단계별로_풀어보기/기본수학/Q1712.java
@@ -0,0 +1,21 @@
+package 백준.단계별로_풀어보기.기본수학;
+
+import java.util.Scanner;
+
+public class BreakEvenPoint {
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+        String[] input = stdIn.nextLine().split(" ");
+        
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int c = Integer.parseInt(input[2]);
+
+        if (c > b){
+            int BreakEvenPoint = (a / (c-b)) + 1;
+            System.out.println(BreakEvenPoint);
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
