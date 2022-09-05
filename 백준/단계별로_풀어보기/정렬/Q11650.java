3ba6e8ae199e873ffcd4164c9a6bebe7baaa0f56
Junghun Sim <96688359+092600@users.noreply.github.com>
2022-09-05 19:32:11 +0900

    2022 09 05 / TIL

---
+++ 백준/단계별로_풀어보기/정렬/Q11650.java
@@ -0,0 +1,98 @@
+//문제
+//2차원 평면 위의 점 N개가 주어진다. 좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+//
+//입력
+//첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N개의 줄에는 i번점의 위치 xi와 yi가 주어진다. (-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 정수이고, 위치가 같은 두 점은 없다.
+//
+//출력
+//첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Q11650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = null;
+
+        int n = Integer.parseInt(br.readLine());
+
+        Integer[][] arr = new Integer[n][2];
+
+        for (int i=0;i<n;i++){
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<Integer[]>() {
+            @Override
+            public int compare(Integer[] o1, Integer[] o2) {
+                if (o1[0].equals(o2[0])){
+                    return o1[1] - o2[1];
+                } else {
+                    return o1[0] - o2[0];
+                }
+            }
+        });
+
+        for(int i=0; i<n; i++) {
+            sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+}
+//public class Q11650 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//
+//        int n = Integer.parseInt(br.readLine());
+//
+//        String[] lst = new String[n];
+//
+//        for (int i=0;i<n;i++){
+//            lst[i] = br.readLine();
+//        }
+//
+//        lst = summaryList(lst);
+//
+//        for (String i : lst){
+//            sb.append(i).append("\n");
+//        }
+//        System.out.println(sb);
+//    }
+//
+//    public static String[] summaryList(String[] lst){
+//        for (int i=0;i<lst.length;i++){
+//            for (int j=i+1; j<lst.length;j++){
+//                if (Integer.parseInt(lst[i].split(" ")[0]) > Integer.parseInt(lst[j].split(" ")[0])){
+//                    String tmp = lst[i];
+//                    lst[i] = lst[j];
+//                    lst[j] = tmp;
+//                }
+//            }
+//        }
+//
+//        for (int i=0;i<lst.length;i++){
+//            for (int j=i+1; j<lst.length;j++){
+//                if (Integer.parseInt(lst[i].split(" ")[0]) == Integer.parseInt(lst[j].split(" ")[0])){
+//                    if (Integer.parseInt(lst[i].split(" ")[1]) > Integer.parseInt(lst[j].split(" ")[1])){
+//                        String tmp = lst[i];
+//                        lst[i] = lst[j];
+//                        lst[j] = tmp;
+//                    }
+//                }
+//            }
+//        }
+//
+//        return lst;
+//    }
+//}
