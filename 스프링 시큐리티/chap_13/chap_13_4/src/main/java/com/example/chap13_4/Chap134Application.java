a1b314887e9ab954d20252212bce2ae4e61796e9
Junghun Sim <96688359+092600@users.noreply.github.com>
2022-10-27 11:52:51 +0900

    2022 10 23 / TIL
    append 스프링 시큐리티/chap13, 14

---
+++ 스프링 시큐리티/chap_13/chap_13_4/src/main/java/com/example/chap13_4/Chap134Application.java
@@ -0,0 +1,13 @@
+package com.example.chap13_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap134Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap134Application.class, args);
+    }
+
+}
---
+++ 스프링 시큐리티/chap_14/chap_14_2/src/main/java/com/example/chap_14_2/Chap142Application.java
@@ -0,0 +1,13 @@
+package com.example.chap_14_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap142Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap142Application.class, args);
+    }
+
+}
