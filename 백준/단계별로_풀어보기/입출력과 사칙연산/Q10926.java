b0bc2df6f71d28dd8c50dc47c2752e7c5313b1db
Junghun Sim <96688359+092600@users.noreply.github.com>
2022-10-29 19:03:45 +0900

    2022 10 29 / TIL

--- 백준/단계별로_풀어보기/입출력과 사칙연산/Q10926.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-class Q10926 {
-    public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
-        String b = s.nextLine();
-        System.out.println(b+"??!");
-    }
-}
--- 백준/단계별로_풀어보기/입출력과 사칙연산/Q2557.java
+++
@@ -1,8 +0,0 @@
-package 백준.단계별로_풀어보기;
-
-public class Q2557 {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-
-    }
-}
